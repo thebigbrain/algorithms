05e6c6b2074bee0de52da874e12e6393f0238196
thebigbrain <379565986@qq.com>
2023-10-29 12:40:56 +0800

    naive prime factorization

--- algs/prime/factorization.py
+++ algs/prime/factorization.py
@@ -1,8 +1,7 @@
+import math
 import unittest
 
-from algs.prime.iterator import PrimeIterator
 from algs.prime.prime import is_prime
-from helpers.runner import run_with_elapse
 from helpers.solution import Solution
 
 
@@ -15,24 +14,16 @@ class PrimeFactorization(Solution):
 
 
 class NaivePrimeFactorization(PrimeFactorization):
-    _iter: PrimeIterator
-
     def calculate(self, n: int) -> [int]:
         result = []
-        while n >= 2:
-            self._iter = PrimeIterator()
-            p = self.find_prime_factor(n)
-            n = n / p
-            result.append(p)
-
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:  # 如果 i 能够整除 n，说明 i 为 n 的一个质因子。
+                while n % i == 0:
+                    n //= i
+                    result.append(i)
+        if n != 1:  # 说明再经过操作之后 n 留下了一个素数
+            result.append(n)
         return result
-
-    def find_prime_factor(self, n: int):
-        p = self._iter.next()
-        while not is_prime_factor(n, p):
-            p = self._iter.next()
-
-        return p
 
 
 class TestFactorization(unittest.TestCase):
@@ -46,8 +37,11 @@ class TestFactorization(unittest.TestCase):
         self.assertEqual([5], prime_factorization(5))
         self.assertEqual([19], prime_factorization(19))
         self.assertEqual([2, 2, 5], prime_factorization(20))
+        self.assertEqual([2, 353, 283286119], naive.calculate(200000000014))
+        self.assertEqual(True, 2 * 253 * 283286119 * 2 > 200000000014)
 
         self.assertEqual(True, is_prime(941))
         self.assertEqual(True, is_prime(10627))
+        self.assertEqual(True, is_prime(283286119))
 
-        run_with_elapse(prime_factorization, 20000014)
+
